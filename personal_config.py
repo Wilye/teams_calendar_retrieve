960eb02649f9fb6c1f396f70554ef56095a52fbd
Shelby <shelbyyang.23@gmail.com>
2023-07-19 14:25:48 -0500

    secured variables

--- personal_config.py
+++
@@ -1,7 +0,0 @@
-# your Azure AD registered app's id and secret
-client_id = '7d844cd4-d153-4b2b-a438-8839061e2035'
-client_secret = 'ZYS8Q~g7AnAm~.s1HJDI~4mYH5DFgaD5uZ-WRczJ'
-tenant_id = '5ce202cb-b98c-4a2c-b703-9495a1d48b51'
-
-group_id = 'd053caff-d520-4b1b-a294-2743fa694c8a'
-channel_id = '19:1e0fh5m1C_1dHyjQh9Q-itLcgoA5cIe7JdJvc46hqOk1@thread.tacv2'
